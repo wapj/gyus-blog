2ca930720a08200ad81061ecef41e6efd14a698c
andy.sg <andy.sg@kakaoent.com>
2023-04-07 18:23:01 +0900

    튜토리얼2 하는중 https://www.gatsbyjs.com/docs/tutorial/part-2/

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import { Link } from "gatsby";
 
 const AboutPage: React.FC<PageProps> = () => {
   return (
     <main>
       <h1>승귤은 누구인가?</h1>
+      <Link to="/">홈으로</Link>
       <p>2022 업데이트 </p>
       <p>동경, 선릉, 부산, 판교 시즌 4 : 카카오엔터테인먼트에서 백엔드 개발자로 활동중</p>
       <p>무엇이든 물어보세요~! https://asked.kr/wapj2000</p>
